2f06d11b7300132b34374a36800f8901ea3a0324
germainsafari <GG@ee10725997111>
2024-09-12 19:07:40 +0200

    Add project files.

---
+++ WebApplication1/Models/TaskModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        [Required]
+        public string AssignedUser { get; set; }
+
+        [Required]
+        public string Priority { get; set; } // High, Medium, Low
+
+        public DateTime CreationDate { get; set; } = DateTime.Now;
+    }
+}
